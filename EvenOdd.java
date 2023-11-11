17fc10c8cf5ed94a7cbb0018fde0b44435b3ee7d
Jose Zeidan <100710528+josezeidan@users.noreply.github.com>
2023-11-11 10:40:45 -0500

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,32 @@
+package interviewPractice;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Please enter an Integer");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		System.out.println(evenodd(num));
+		
+		
+	}
+	
+	public static String evenodd(int num) {
+		
+		
+		if(num%2 == 0) 
+			return num+" is Even";
+		else
+			return num+" is Odd";
+		
+	}
+
+}
